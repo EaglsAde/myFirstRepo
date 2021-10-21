0e7484d9a9a6770c8c6a4d8ffdd5656abd7f14cd
Adel <kramatullahadel48@gmail.com>
2021-10-21 17:03:34 -0400

    my first git commit

---
+++ src/main/java/core/Base.java
@@ -0,0 +1,12 @@
+package core;
+
+import java.util.Properties;
+
+import org.openqa.selenium.WebDriver;
+
+public class Base {
+	public static WebDriver driver;
+	public static Properties properties;
+	private static Logger Logger;
+
+}
